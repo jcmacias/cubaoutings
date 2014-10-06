ca0e402a84930cfa268c58d011dd682f19a9584e
julio <jcesar@jovenclub.cu>
2014-10-06 09:57:30 -0400

    Cambio background

--- protected/config/main.php
+++ protected/config/main.php
@@ -27,12 +27,12 @@ return array(
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
 
-		'gii'=>array(
+		/*'gii'=>array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>'123456',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','192.168.22.41','::1'),
-		),
+		),*/
 
 	),
 
