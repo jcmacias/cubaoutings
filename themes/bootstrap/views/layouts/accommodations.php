38743f24a56b38d7daec21ff9e9618db55a23e01
julio <jcesar@jovenclub.cu>
2015-09-10 12:14:30 -0700

    eliminada la notificacion de oferta temporalmete hacerlo desde la administracion

--- themes/bootstrap/views/layouts/accommodations.php
+++ themes/bootstrap/views/layouts/accommodations.php
@@ -120,38 +120,38 @@
 <?php $this->endContent(); ?>
 </div>
 <?php
-$datos=Offer::model()->GetOffer();
-if($datos!=null ){
-    if(Yii::app()->getLanguage() == 'en'){
-        $message='<table><td>'.CHtml::image(Yii::app()->baseUrl . '/images/offer_image/'.$datos[0]->imagen, 'Cuba',array('class'=>'img-circle','style'=>'width:70px;')).'</td><td><p style="padding-left:7px">'.$datos[0]->message.'</p></td></table>';
-    }
-    if(Yii::app()->getLanguage() == 'fr'){
-        $message='<table><td>'.CHtml::image(Yii::app()->baseUrl . '/images/offer_image/'.$datos[0]->imagen, 'Cuba',array('class'=>'img-circle','style'=>'width:70px;')).'</td><td><p style="padding-left:7px">'.$datos[0]->message_fr.'</p></td></table>';
-    }
-    if(Yii::app()->getLanguage() == 'es'){
-        $message='<table><td>'.CHtml::image(Yii::app()->baseUrl . '/images/offer_image/'.$datos[0]->imagen, 'Cuba',array('class'=>'img-circle','style'=>'width:70px;')).'</td><td><p style="padding-left:7px">'.$datos[0]->message_es.'</p></td></table>';
-    }
-    if(Yii::app()->getLanguage() == 'it'){
-        $message='<table><td>'.CHtml::image(Yii::app()->baseUrl . '/images/offer_image/'.$datos[0]->imagen, 'Cuba',array('class'=>'img-circle','style'=>'width:70px;')).'</td><td><p style="padding-left:7px">'.$datos[0]->message_it.'</p></td></table>';
-    }
-    if(Yii::app()->getLanguage() == 'ru'){
-        $message='<table><td>'.CHtml::image(Yii::app()->baseUrl . '/images/offer_image/'.$datos[0]->imagen, 'Cuba',array('class'=>'img-circle','style'=>'width:70px;')).'</td><td><p style="padding-left:7px">'.$datos[0]->message_ru.'</p></td></table>';
-    }
-//echo '<pre>';
-//print_r($message[0]->message);
-//echo '</pre>';die;
-    if(Yii::app()->user->isGuest  ){
-        $this->widget('application.extensions.PNotify.PNotify',array(
-                'options'=>array(
-                    // 'title'=>'Special offer!!!',
-                    'text'=>$message,
-                    'type'=>'success',
-                    'closer'=>true,
-                    'sticker'=>false,
-                    'icon'=>false,
-                    'hide'=>false))
-        );
-    }
-}
+//$datos=Offer::model()->GetOffer();
+//if($datos!=null ){
+//    if(Yii::app()->getLanguage() == 'en'){
+//        $message='<table><td>'.CHtml::image(Yii::app()->baseUrl . '/images/offer_image/'.$datos[0]->imagen, 'Cuba',array('class'=>'img-circle','style'=>'width:70px;')).'</td><td><p style="padding-left:7px">'.$datos[0]->message.'</p></td></table>';
+//    }
+//    if(Yii::app()->getLanguage() == 'fr'){
+//        $message='<table><td>'.CHtml::image(Yii::app()->baseUrl . '/images/offer_image/'.$datos[0]->imagen, 'Cuba',array('class'=>'img-circle','style'=>'width:70px;')).'</td><td><p style="padding-left:7px">'.$datos[0]->message_fr.'</p></td></table>';
+//    }
+//    if(Yii::app()->getLanguage() == 'es'){
+//        $message='<table><td>'.CHtml::image(Yii::app()->baseUrl . '/images/offer_image/'.$datos[0]->imagen, 'Cuba',array('class'=>'img-circle','style'=>'width:70px;')).'</td><td><p style="padding-left:7px">'.$datos[0]->message_es.'</p></td></table>';
+//    }
+//    if(Yii::app()->getLanguage() == 'it'){
+//        $message='<table><td>'.CHtml::image(Yii::app()->baseUrl . '/images/offer_image/'.$datos[0]->imagen, 'Cuba',array('class'=>'img-circle','style'=>'width:70px;')).'</td><td><p style="padding-left:7px">'.$datos[0]->message_it.'</p></td></table>';
+//    }
+//    if(Yii::app()->getLanguage() == 'ru'){
+//        $message='<table><td>'.CHtml::image(Yii::app()->baseUrl . '/images/offer_image/'.$datos[0]->imagen, 'Cuba',array('class'=>'img-circle','style'=>'width:70px;')).'</td><td><p style="padding-left:7px">'.$datos[0]->message_ru.'</p></td></table>';
+//    }
+////echo '<pre>';
+////print_r($message[0]->message);
+////echo '</pre>';die;
+//    if(Yii::app()->user->isGuest  ){
+//        $this->widget('application.extensions.PNotify.PNotify',array(
+//                'options'=>array(
+//                    // 'title'=>'Special offer!!!',
+//                    'text'=>$message,
+//                    'type'=>'success',
+//                    'closer'=>true,
+//                    'sticker'=>false,
+//                    'icon'=>false,
+//                    'hide'=>false))
+//        );
+//    }
+//}
 
 ?>
